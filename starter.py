add482ef6b6753f3f81837a91e123d6a6bd25e92
Aleksandr Kasatkin <aleksandr_kasatkin@epam.com>
2026-01-30 21:48:58 +0100

    finished

--- starter.py
+++ starter.py
@@ -4,9 +4,17 @@ import asyncio
 # if __name__ == "__main__":
 #     asyncio.run(main())
 
-from task.t2.input_api_based import main
+# from task.t2.input_api_based import main
+# if __name__ == "__main__":
+#     main()
+
+# from task.t2.input_vector_based import main
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
+from task.t3.in_out_grounding import main
 if __name__ == "__main__":
-    main()
-    
+    asyncio.run(main())
+
 # Start the user service: docker-compose up -d
 # python starter.py
